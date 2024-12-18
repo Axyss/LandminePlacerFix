dd1d3a05415fb541372c46edd782cdf425f027a2
Axyss <alexnieto201@gmail.com>
2024-12-18 21:19:32 +0100

    rebranded metadata and bumped dependencies

--- LandminePlacer/PluginInfo.cs
+++ LandminePlacer/PluginInfo.cs
@@ -2,8 +2,8 @@ namespace LandminePlacer
 {
     public static class PluginInfo
     {
-        public const string PluginGuid = "alexandre_v1.LandminePlacer";
-        public const string PluginName = "Landmine Placer";
-        public const string PluginVersion = "1.0.1";
+        public const string PluginGuid = "Axyss.LandminePlacerFix";
+        public const string PluginName = "Landmine Placer Fix";
+        public const string PluginVersion = "1.0.2";
     }
 }
